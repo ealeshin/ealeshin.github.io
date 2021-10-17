3c5f2c36c489c84b09670788ac9ab8f8adfbeaf0
Evgeny Aleshin <evgenyaleshin@icloud.com>
2021-10-17 15:51:10 +0300

    Site pages layout

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
-const pageHolder = document.querySelector('#page');
 const menuIcon = document.querySelector('#menuToggle');
 
 const mobileMenu = document.querySelectorAll('.nav-container')[0];
 const mainContent = document.querySelectorAll('.intro-container')[0];
 
-const route = (page) => {
-    pageHolder.innerHTML = 'Location <span>' + page + '</span> is opening soon';
-};
+const nav = document.querySelectorAll('nav > a');
+const pages = document.querySelectorAll('main, section');
 
 menuIcon.addEventListener('click', () => {
     let menuOpened = menuIcon.classList.contains('opened');
@@ -19,17 +17,21 @@ menuIcon.addEventListener('click', () => {
     }
 });
 
-const nav = document.querySelectorAll('nav > a');
 nav.forEach((link) => {
     link.addEventListener('click', (event) => {
         event.preventDefault();
-        pageHolder.classList.add("fade-in");
-        setTimeout(() => {
-            pageHolder.classList.remove("fade-in");
-        }, 2500);
-        //route(link.getAttribute('data-page'));
-        route(link.innerHTML);
+        let clickedLink = event.target;
+        nav.forEach((navLink) => {
+            navLink.classList.remove('active');
+        });
+        clickedLink.classList.add('active');
+        pages.forEach((page) => {
+            page.style.display = 'none';
+        });
+        let activePageName = clickedLink.getAttribute('data-page');
+        let activePage = document.querySelector('#' + activePageName);
+        if (activePage !== null) {
+            activePage.style.display = 'block';
+        }
     });
 });
-
-route(window.location.pathname);
